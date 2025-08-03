d3ee62f59bd66cb00c42ec292149c9b12f681179
Cartoshechka <skalatushkin@gmail.com>
2025-08-03 12:23:32 -0500

    correcting console output

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ import {getRandomInt} from './getRandomInt.js';
 import {greetByName} from './greetByName.js'
 import {sumBigIntegers} from './sumBigIntegers.js'
 
-global.myFunctions = { getRandomInt, greetByName, sumBigIntegers };
-
+console.log('main.js\n')
+console.log('------------getRandomInt.js----------------------')
 console.log(getRandomInt(1, 10));
 console.log(getRandomInt(40, 50));
 console.log(getRandomInt(1, 100));
-console.log('-----------------------------------')
+console.log('\n------------greetByName.js-----------------------')
 console.log(greetByName('Hi', 'John')) // Hi, John
 console.log(greetByName('Hey', 'Bob')) // Hey, Bob
 console.log(greetByName('Hello', 'Mary')) // Hello, Mary
-console.log('-----------------------------------')
+console.log('\n------------sumBigIntegers.js---------------------')
 console.log(sumBigIntegers('9007199254740991', '9007199254740991')); // виводить 18014398509481982n
